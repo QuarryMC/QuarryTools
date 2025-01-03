e307d67b6987f92e5d3ceaa41713a100f2c8c9ae
Celllsnide <nwerickson09@gmail.com>
2025-01-03 11:57:21 -0600

    Codex GUI

--- src/main/java/codes/kooper/quarryTools/commands/CodexCommand.java
+++ src/main/java/codes/kooper/quarryTools/commands/CodexCommand.java
@@ -1,6 +1,7 @@
 package codes.kooper.quarryTools.commands;
 
 import codes.kooper.quarryTools.guis.CodexGUI;
+import codes.kooper.shaded.litecommands.annotations.argument.Arg;
 import codes.kooper.shaded.litecommands.annotations.command.Command;
 import codes.kooper.shaded.litecommands.annotations.execute.Execute;
 import codes.kooper.shaded.litecommands.annotations.permission.Permission;
@@ -11,8 +12,7 @@ import org.bukkit.entity.Player;
 public class CodexCommand {
 
     @Execute
-    public void openCodex(Player player) {
+    public void openCodex(@Arg Player player) {
         new CodexGUI(player);
     }
 }
-//
